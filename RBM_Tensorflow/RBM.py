dc8043fe311db2c30ab9b187d55b1ea84ca901d5
Ali Mashhoori <a.mashhoori@gmail.com>
2017-02-26 18:39:45 -0700

    RBM Updated

--- RBM_Tensorflow/RBM.py
+++ RBM_Tensorflow/RBM.py
@@ -1,123 +1,187 @@
 import tensorflow as tf
+import numpy as np
+import os
 
-class RBM:
-    
-    def __init__(self, numHidden, numVisible, numSteps = 1):
+class RBM:    
+    def __init__(self, numHidden, numVisible, numSteps = 1, pcd = False):
         
         self._numHidden = numHidden
         self._numVisible = numVisible
         self._numSteps = numSteps
+        self._pcd = pcd
         
-        self.w = tf.Variable(tf.truncated_normal([self._numVisible, self._numHidden], stddev=0.01), name = 'weights')
-        self.b = tf.Variable(tf.zeros([self._numVisible]), name = 'vBias')
-        self.c = tf.Variable(tf.zeros([self._numHidden]), name = 'vHidden')        
-        
-        self.saver = tf.train.Saver()    
-        self.savePath = r'.\model'
-        
-        self.DefineComputationalGraph()
-        self.DefineLearningParameters()          
-        
-    #############################################################################################    
-    def sample_prob(self, probs, rand):
-        return tf.nn.relu(tf.sign(probs - rand))
+        self.graph = tf.Graph()         
+        with self.graph.as_default(): 
+            self._define_parameters()        
+            self._define_computational_graph()
+            self._define_learning_parameters()        
+            self._saver = tf.train.Saver()
+            self._init = tf.global_variables_initializer()                      
    
+        self.sess = tf.Session( graph = self.graph )        
+        self.sess.run(self._init)
+        
+    #############################################################################################        
+    def _define_parameters(self):        
+        
+        low = -4 * np.sqrt(6.0/(self._numVisible + self._numHidden))
+        high = -low
+        
+        self._w = tf.Variable(tf.random_uniform(shape=[self._numVisible, self._numHidden], minval=low, maxval=high), dtype=tf.float32, name = 'weights')
+        self._b = tf.Variable(tf.zeros([self._numVisible]), name = 'vBias')
+        self._c = tf.Variable(tf.zeros([self._numHidden]), name = 'vHidden')
+        
     #############################################################################################    
-    def DefineComputationalGraph(self):          
+    def _define_computational_graph(self):
         
-        self.visible = tf.placeholder(tf.float32, [None, self._numVisible], name='visible')
+        self._visible = tf.placeholder(tf.float32, [None, self._numVisible], name='visible')        
+        self._visible_random_nums = tf.random_uniform(tf.shape(self._visible), name = 'vRand')
+        self._hidden_random_nums  = tf.random_uniform([tf.shape(self._visible)[0], self._numHidden] , name='hRand')                
+        self._hidden, self._bhidden = self._sample_hidden_from_visible(self._visible)                
         
-        self.visibleRandomNums = tf.random_uniform(tf.shape(self.visible), name = 'vRand')
-        self.hiddenRandomNums  = tf.random_uniform([tf.shape(self.visible)[0], self._numHidden] , name='hRand')     
-        
-        self.hidden, self.bhidden = self.Sample_Hidden_From_Visible(self.visible)                
-       
-        def body(a, bhidden):            
-            a = tf.add(a, 1)            
-            bNewVisible = self.Sample_Visible_From_Hidden(bhidden)[-1]
-            bhidden = self.Sample_Hidden_From_Visible(bNewVisible)[-1]     
+        def body(a, bhidden):
+            a = tf.add(a, 1)
+            bNewVisible = self._sample_visible_from_hidden(bhidden)[-1]
+            bhidden = self._sample_hidden_from_visible(bNewVisible)[-1]
             return a, bhidden            
         condition = lambda a, b: tf.less(a, self._numSteps-1)
         
-        bhidden = tf.while_loop(condition, body, [0, self.bhidden])[1]        
+        if(self._pcd):
+            self._flip_index = 0
+            self._steady = tf.Variable( tf.random_uniform([100, self._numVisible]), name= 'steadyState') 
+            bhiddenFromSteady = self._sample_hidden_from_visible(self._steady)[1]            
+            bhidden = tf.while_loop(condition, body, [0, bhiddenFromSteady])[1]   
+        else:
+            bhidden = tf.while_loop(condition, body, [0, self._bhidden])[1]                            
         
-        self.newVisible, self.bNewVisible = self.Sample_Visible_From_Hidden(bhidden)
-        self.newHidden = tf.nn.sigmoid(tf.matmul(self.bNewVisible, self.w) + self.c, name='newHiddenProb')
-        
-        self.cost = -tf.reduce_mean(tf.mul(self.visible, tf.log(self.newVisible)))
-
+        self._new_visible, self._b_new_visible = self._sample_visible_from_hidden(bhidden) 
+        self._new_hidden = tf.nn.sigmoid(tf.matmul(self._b_new_visible, self._w) + self._c, name='newHiddenProb')        
+            
+        if(self._pcd):
+            self._save_chain_state = self._steady.assign(self._b_new_visible)  
+            self._cost = self._get_pseudo_likelihood_cost()
+        else:
+            self._cost = -tf.reduce_mean(tf.mul(self._visible, tf.log(self._new_visible)))
+    
     #############################################################################################    
-    def DefineLearningParameters(self):
+    def _define_learning_parameters(self):    
         
-        dw = ((tf.matmul(self.visible, self.hidden, transpose_a=True) - tf.matmul(self.bNewVisible, self.newHidden, transpose_a=True)) / tf.cast(tf.shape(self.visible)[0], dtype= tf.float32))
-        db =  tf.reduce_mean(self.visible - self.bNewVisible, axis = 0)
-        dc =  tf.reduce_mean(self.hidden - self.newHidden, axis = 0)
+        dw = ((tf.matmul(self._visible, self._hidden, transpose_a=True) - tf.matmul(self._b_new_visible, self._new_hidden, transpose_a=True)) / tf.cast(tf.shape(self._visible)[0], dtype= tf.float32))
+        db =  tf.reduce_mean(self._visible - self._b_new_visible, axis = 0)
+        dc =  tf.reduce_mean(self._hidden - self._new_hidden, axis = 0)
        
         velw = tf.Variable(tf.zeros([self._numVisible, self._numHidden], dtype= tf.float32), name = 'velWeights')
         velb = tf.Variable(tf.zeros([self._numVisible], dtype= tf.float32), name = 'velVBias')
         velc = tf.Variable(tf.zeros([self._numHidden], dtype= tf.float32), name = 'velHBias')
         
-        self.learningRate = tf.constant(0.05)
+        self.learningRate = tf.constant(0.01)
         self.momentum = tf.constant(0.9)
          
-        self.updateVelW = velw.assign(self.momentum * velw + self.learningRate * dw)
-        self.updateVelB = velb.assign(self.momentum * velb + self.learningRate * db)
-        self.updateVelC = velc.assign(self.momentum * velc + self.learningRate * dc)
+        updateVelW = velw.assign(self.momentum * velw + self.learningRate * dw)
+        updateVelB = velb.assign(self.momentum * velb + self.learningRate * db)
+        updateVelC = velc.assign(self.momentum * velc + self.learningRate * dc)
         
-        self.updateW = self.w.assign_add(velw)
-        self.updateB = self.b.assign_add(velb)
-        self.updateC = self.c.assign_add(velc)
-
-    #############################################################################################    
-    def SetSession(self, sess):
-        self.sess = sess
+        with tf.control_dependencies([updateVelW, updateVelB, updateVelC]):
+            self._updateW = self._w.assign_add(velw)
+            self._updateB = self._b.assign_add(velb)
+            self._updateC = self._c.assign_add(velc)
+        
+    #############################################################################################
+    def _free_energy(self, v_sample):        
+        
+        wx_c = tf.matmul(v_sample, self._w) + self._c
+        vbias_term = tf.matmul(v_sample, tf.reshape(self._b, [self._numVisible, 1]))
+        hidden_term = tf.reduce_sum(tf.log(1 + tf.exp(wx_c)), axis=1)
+        return -hidden_term - vbias_term
     
     #############################################################################################    
-    def Initilize(self):
-        init = tf.global_variables_initializer()
-        self.sess.run(init)
+    def _sample_prob(self, probs, rand):
+        
+        return tf.nn.relu(tf.sign(probs - rand))
+        
+    #############################################################################################
+    def _get_pseudo_likelihood_cost(self):         
+        
+        self._flipIndex = tf.placeholder(tf.int32, shape=())        
+        xi = self._visible       
+        fe_xi = self._free_energy(xi)
+               
+        part1 = tf.slice(xi, [0, 0], [-1 , self._flipIndex])
+        part2 = tf.slice(xi, [0, self._flipIndex], [-1, 1])
+        part3 = tf.slice(xi, [0, self._flipIndex+1], [-1, -1])   
+           
+        xi_flip =  tf.concat(values = [part1, 1 - part2, part3], concat_dim =1)        
+        fe_xi_flip = self._free_energy(xi_flip)        
+        cost = tf.reduce_mean(self._numVisible * tf.log(tf.nn.sigmoid(fe_xi_flip - fe_xi))) 
+        return cost
         
     #############################################################################################    
-    def TrainForBatch(self, inputData):
-        result = self.sess.run([self.newVisible, self.updateVelW, self.updateVelW, self.updateVelW, self.cost], feed_dict = {self.visible : inputData})
-        self.sess.run([self.updateW, self.updateB, self.updateC])        
+    def train_for_batch(self, inputData): 
+        
+        if(self._pcd):
+            result = self.sess.run([self._new_visible, self._updateW, self._updateB, self._updateC, self._save_chain_state, self._cost], feed_dict = {self._visible : inputData, self._flipIndex:self._flip_index})
+            self._flip_index = (self._flip_index + 1) % self._numVisible        
+        else:   
+            result = self.sess.run([self._new_visible, self._updateW, self._updateB, self._updateC, self._cost], feed_dict = {self._visible : inputData})           
         return result[-1]
 
     #############################################################################################    
-    def SaveTheModel(self):
-        self.saver.save(self.sess, self.savePath)
+    def save_model(self, saveFolder = r'.\model', name = 'model'):  
+        
+        if(not os.path.exists(saveFolder)):
+            os.mkdir(saveFolder)        
+        path = os.path.join(saveFolder, name)            
+        self._saver.save(self.sess, path)
         
     #############################################################################################    
-    def LoadModel(self):
-        self.Initilize()
-        self.saver.restore(self.sess, self.savePath)
-             
+    def load_model(self, saveFolder = r'.\model', name = 'model'):
+        
+        self.sess.run(self._init)        
+        path = os.path.join(saveFolder, name)            
+        self._saver.restore(self.sess, path) 
+        
+    #############################################################################################    
+    def __del__(self):
+        self.close()
+    
+    #############################################################################################    
+    def close(self):
+        self.sess.close()         
+        
     #############################################################################################                                                  
-    def Sample_Hidden_From_Visible(self, visible):
-        hidden = tf.nn.sigmoid(tf.matmul(visible, self.w) + self.c, name='hiddenProb')
-        bhidden =  self.sample_prob(hidden, self.hiddenRandomNums)
+    def _sample_hidden_from_visible(self, visible):
+        
+        hidden = tf.nn.sigmoid(tf.matmul(visible, self._w) + self._c, name='hiddenProb')
+        bhidden =  self._sample_prob(hidden, self._hidden_random_nums)
         return hidden, bhidden
         
     #############################################################################################    
-    def Sample_Visible_From_Hidden(self, hidden):
-        newVisible = tf.nn.sigmoid(tf.matmul(hidden, self.w, transpose_b=True) + self.b, name='newVisibleProb')           
-        bNewVisible = self.sample_prob(newVisible, self.visibleRandomNums)        
+    def _sample_visible_from_hidden(self, hidden):
+        
+        newVisible = tf.nn.sigmoid(tf.matmul(hidden, self._w, transpose_b=True) + self._b, name='newVisibleProb')           
+        bNewVisible = self._sample_prob(newVisible, self._visible_random_nums)        
         return newVisible, bNewVisible   
         
     #############################################################################################           
-    def GenerateSample(self, visibleData, numberOfSteps): 
+    def generate_sample(self, visibleData, numberOfSteps):    
         
         def body(a, visible):            
             a = tf.add(a, 1)  
-            bhidden = self.Sample_Hidden_From_Visible(visible)[-1]
-            bNewVisible = self.Sample_Visible_From_Hidden(bhidden)[-1] 
-            return a, bNewVisible
-            
-        condition = lambda a, b: tf.less(a, numberOfSteps)
+            bhidden = self._sample_hidden_from_visible(visible)[-1]
+            bNewVisible = self._sample_visible_from_hidden(bhidden)[-1] 
+            return a, bNewVisible            
+        condition = lambda a, b: tf.less(a, numberOfSteps)        
         
-        r = tf.while_loop(condition, body, [0, self.visible])
-        visibleData = self.sess.run(r, feed_dict = {self.visible : visibleData})  
+        r = tf.while_loop(condition, body, [0, self._visible])
+        visibleData = self.sess.run(r, feed_dict = {self._visible : visibleData})  
         return visibleData[1]
+
+    #############################################################################################           
+    def get_parameters(self):
         
-        
-    
+        [w, b, c] = self.sess.run([self._w, self._b, self._c])
+        return {'w': w,
+                'b': b,
+                'c': c                
+                }
+
--- RBM_Tensorflow/main.py
+++ RBM_Tensorflow/main.py
@@ -5,47 +5,47 @@ import RBM
 import matplotlib.pyplot as plt
 
 
-def ConvertToBinary(a):
+def convert_to_binary(a):
     a[a >= 0.5] = 1
     a[a <  0.5] = 0
     return a
     
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-bTrainImages = ConvertToBinary(mnist.train.images)    
+bTrainImages = convert_to_binary(mnist.train.images)
 
 tf.reset_default_graph()
 
-rbm = RBM.RBM(500, 28*28, 1)
-with tf.Session() as sess:    
-    rbm.SetSession(sess)       
-    
-    rbm.Initilize()  
-    for i in range(10000):
-         batch_x, batch_y = mnist.train.next_batch(100)
-         cost = rbm.TrainForBatch(batch_x)         
-         print(cost)
+costTotal = 0
+rbm = RBM.RBM(500, 28*28, 15, True)
+
+for i in range(5000):
+     batch_x, batch_y = mnist.train.next_batch(100)
+     cost = rbm.train_for_batch(batch_x)         
+     costTotal += cost
   
-         if(i % 1000 == 0):
-             print(i)            
-             sample = rbm.GenerateSample(batch_x, 1)
-             for j in range(2):
-                 plt.figure()
-                 plt.imshow(sample[j].reshape([28, 28]))  
+     if(i % (784) == 0):
+         print(i)            
+         print(costTotal/784)
+         costTotal = 0
+         sample = rbm.generate_sample(batch_x, 15)
+         for j in range(2):
+             plt.figure()
+             plt.imshow(sample[j].reshape([28, 28]))
+
              
-    rbm.SaveTheModel()
+params = rbm.get_parameters()             
+rbm.save_model()
+rbm.close()        
 
-    
-    
 batch_x, batch_y = mnist.validation.next_batch(100)
+tf.reset_default_graph()
+rbm2 = RBM.RBM(500, 28*28, 200, True)
+rbm2.load_model()
+    
+sample = rbm2.generate_sample(batch_x, 5000)
+    
+for j in range(2):
+    plt.figure()
+    plt.imshow(sample[j].reshape([28, 28]))
+rbm2.close()
 
-tf.reset_default_graph()    
-rbm2 = RBM.RBM(500, 28*28)
-with tf.Session() as sess:    
-    rbm2.SetSession(sess)       
-    rbm2.LoadModel()         
-    
-    sample = rbm2.GenerateSample(batch_x, 5000)
-    
-    for j in range(2):
-        plt.figure()
-        plt.imshow(sample[j].reshape([28, 28]))  
